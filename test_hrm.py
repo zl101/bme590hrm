cbb066af5dac966a814dba7e8a412d10cfc08a08
Zachary Liu <liu_zachary@live.com>
2018-10-22 22:44:04 -0400

    Voltage Extremes function and CSV reading function

--- test_hrm.py
+++ test_hrm.py
@@ -1,7 +1,7 @@
 from hrm import readCSV,voltageExtremes
 import pytest
 @pytest.mark.parametrize("teststr,expected",[
-    ([0,1,2],(0,2))),
+    ([0,1,2],(0,2)),
     ([-5.5,-10,-2,6], (-10,6))
 ])
 def test(teststr,expected):
---
+++ test_voltageextremes.py
@@ -0,0 +1,11 @@
+from hrm import voltageExtremes
+import pytest
+@pytest.mark.parametrize("testinput,expected",[
+    ([0,1,2],(0,2)),
+    ([-5.5,-10,-2,6], (-10,6)),
+    ([9,-8,-8,11,0.005],(-8,11))
+])
+
+def test(testinput,expected):
+    res = voltageExtremes(testinput)
+    assert res == expected
