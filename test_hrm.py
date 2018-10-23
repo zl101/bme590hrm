0212adaf7cfe6bec8c359b9a5f2d25624925f8f6
Zachary Liu <liu_zachary@live.com>
2018-10-22 22:45:53 -0400

    last commit was missing file

--- test_hrm.py
+++
@@ -1,9 +0,0 @@
-from hrm import readCSV,voltageExtremes
-import pytest
-@pytest.mark.parametrize("teststr,expected",[
-    ([0,1,2],(0,2)),
-    ([-5.5,-10,-2,6], (-10,6))
-])
-def test(teststr,expected):
-    res = voltageExtremes(teststr)
-    assert res == expected
