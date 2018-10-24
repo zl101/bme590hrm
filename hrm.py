7a237c896f4cc01e66165fe5fd8a31e2bfd80317
Zachary Liu <liu_zachary@live.com>
2018-10-23 21:40:13 -0400

    Beat times and beat counting and robustness for csv input

--- hrm.py
+++ hrm.py
@@ -1,6 +1,12 @@
 import csv
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 CONST_MAXVOLTAGE = 10000
 CONST_MINVOLTAGE = -10000
+CONST_MINBPS = 0.5
+CONST_CUTOFF = 0.7
+CONST_MINTIMEBETWEENBEATS = 0.2
 
 
 def readCSV(filename):
@@ -12,14 +18,50 @@ def readCSV(filename):
         timesarr = []
         voltagesarr = []
         for row in reader:
+            if(len(row) != 2):
+                continue
+            if(row[0] == '' or row[1] == ''):
+                continue
             timesarr.append(float(row[0]))
             voltagesarr.append(float(row[1]))
     return [timesarr, voltagesarr]
 
 
 def getDuration(timesarr):
+    """
+    gets the duration of the input time array
+    """
     dur = max(timesarr)-min(timesarr)
     return dur
+
+
+def getBeats(timesarr, voltagesarr):
+    """
+    returns array of times of when there were beats
+    """
+    dur = getDuration(timesarr)
+    numpointsforavg = int(np.ceil(CONST_MINBPS*dur))
+    sortedvoltagesasc = sorted(voltagesarr, reverse=True)
+    sortedvoltagesd = sorted(voltagesarr)
+    cutoff = CONST_CUTOFF * np.average(sortedvoltagesasc[0:numpointsforavg])
+    cutoff += np.average(sortedvoltagesd[0:numpointsforavg]) * (1-CONST_CUTOFF)
+    length = len(timesarr)
+    beats = []
+    lastbeat = -1
+    for x in range(0, length-2):
+        if(voltagesarr[x+1] > cutoff and voltagesarr[x+1] > voltagesarr[x] and
+                voltagesarr[x+1] > voltagesarr[x+2] and
+                timesarr[x+1]-lastbeat > CONST_MINTIMEBETWEENBEATS):
+            beats.append(timesarr[x+1])
+            lastbeat = timesarr[x+1]
+    return np.array(beats)
+
+
+def getNumBeats(beatsarr):
+    """
+    given array of times at which beats occur, returns total number of beats
+    """
+    return len(beatsarr)
 
 
 def voltageExtremes(voltages):
@@ -36,4 +78,9 @@ def voltageExtremes(voltages):
     return (min, max)
 
 if __name__ == "__main__":
-        [time, voltage] = readCSV('test_data1.csv')
+        [time, voltage] = readCSV('test_data11.csv')
+        beattimes = getBeats(time, voltage)
+        print(getNumBeats(beattimes))
+        fig, ax = plt.subplots()
+        ax.plot(time, voltage)
+        plt.show()
